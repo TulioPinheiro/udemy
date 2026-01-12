39ed1bcd0daa6906c7a111c4ddc59ce530d72cf2
Túlio Pinheiro <tuliopfreitas31@gmail.com>
2026-01-12 00:32:06 -0300

    removendo espaço desnecessario

--- aula68.py
+++ aula68.py
@@ -5,5 +5,4 @@ Existe o escopo global e local.
 O escopo global é o escopo onde todo o código é alcançavel.
 O escopo local é o escopo onde apenas nomes do mesmo local 
 podem ser alcançados.
-
 """
